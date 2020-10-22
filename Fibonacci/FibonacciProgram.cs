f85cc951bc1875d0687a5a7c120bad91d8760e1c
Mathieu Lamoureux <30454625+MathieuLamoureux@users.noreply.github.com>
2020-10-21 22:13:06 -0400

    Created FibonacciProgram.cs
    Created program without sequence and display logic.

---
+++ Fibonacci/FibonacciProgram.cs
@@ -0,0 +1,16 @@
+using System;
+public class FibonacciProgram
+{
+    public static void Main()
+    {
+        int userNum;
+        Console.WriteLine("Enter number: ");
+        userNum = Convert.ToInt32(Console.ReadLine());
+        //call function here
+        //display sequence array here
+    }
+    public static int[] FibonacciSequence(int x)
+    {
+       //function logic here
+    }
+}
