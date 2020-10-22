047d5098da70aa4816d2e720b2048bb16f3dc24c
Mathieu Lamoureux <30454625+MathieuLamoureux@users.noreply.github.com>
2020-10-21 21:42:32 -0400

    Create TriangleProgram file
    Create program without calculation logic.

---
+++ Triangle/TriangleProgram.cs
@@ -0,0 +1,18 @@
+using System;
+public class TriangleProgram
+{
+    public static void Main()
+    {
+        double triBase;
+        double triHeight;
+        double area;
+
+        Console.WriteLine("Enter base: ");
+        triBase = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter height: ");
+        triHeight = Convert.ToDouble(Console.ReadLine());
+        //call function here
+        Console.WriteLine("Triangle area is: " + area);
+    }
+    //function logic here
+}
